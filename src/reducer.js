96b1dce9edc03adf2693f33e3eafa4e198c55514
Michael Williams <michael.williams@enspiral.com>
2016-04-25 18:33:19 +1200

    wip

--- src/reducer.js
+++ src/reducer.js
@@ -1,39 +1,178 @@
 'use strict'
 
-const handleActions = require('redux-actions').handleActions
+const handleActions = require('redux-actions/lib/handleActions')
 const mapValues = require('lodash/mapValues')
+const assign = require('lodash/assign')
+const isUndefined = require('lodash/isUndefined')
+const values = require('lodash/values')
+const flatten = require('lodash/flatten')
 
-const createActionPayloadType = require('./types').createActionPayloadType
 const createActionTypes = require('./action-types')
-const Resource = require('./types').Resource
+const createPayloadTypes = require('./payload-types')
+const types = require('./types')
 const constants = require('./constants')
 
-const Options = Tc.struct({
+const specCreators = {
+  find: {
+    start: function (action) {
+      return startSpec(action)
+    },
+    success: function (action) {
+      return assign({
+        records: action.payload.result.reduce(function (sofar, entity) {
+          sofar[entity[key]] = { $set: entity }
+          return sofar
+        }, {})
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return errorSpec(action)
+    }
+  },
+  get: {
+    start: function (action) {
+      return startSpec(action)
+    },
+    success: function (action) {
+      return assign({
+        records: {
+          [action.payload.id]: { $set: action.payload.result }
+        }
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return errorSpec(action)
+    }
+  },
+  create: {
+    start: function (action) {
+      return assign({
+        records: {
+          [action.payload.cid]: { $set: action.payload.data }
+        }
+      }, startSpec(action))
+    },
+    success: function (action) {
+      return assign({
+        records: {
+          $remove: [action.payload.cid],
+          [action.payload.result[key]]: { $set: action.payload.result }
+        }
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return assign({
+        records: {
+          $remove: [action.payload.cid]
+        }
+      }, errorSpec(action))
+    }
+  },
+  update: {
+    start: function (action) {
+      return assign({
+        records: {
+          [action.payload.id]: { $set: action.payload.data }
+        }
+      }, startSpec(action))
+    },
+    success: function (action) {
+      return assign({
+        records: {
+          [action.payload.id]: { $set: action.payload.result }
+        }
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return assign({
+        // TODO reset to before update
+        records: {
+          $remove: [action.payload.id]
+        }
+      }, errorSpec(action))
+    }
+  },
+  patch: {
+    start: function (action) {
+      return assign({
+        records: {
+          [action.payload.id]: { $merge: action.payload.data }
+        }
+      }, startSpec(action))
+    },
+    success: function (action) {
+      return assign({
+        records: {
+          [action.payload.id]: { $merge: action.payload.result }
+        }
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return assign({
+        // TODO reset to before patch
+        records: {
+          $remove: [action.payload.id]
+        }
+      }, errorSpec(action))
+    }
+  },
+  remove: {
+    start: function (action) {
+      return assign({
+        records: {
+          $remove: [action.payload.id]
+        }
+      }, startSpec(action))
+    },
+    success: function (action) {
+      return assign({
+        records: {
+          $remove: [action.payload.id]
+        }
+      }, successSpec(action))
+    },
+    error: function (action) {
+      return assign({
+        // TODO reset to before remove
+        records: {
+          $remove: [action.payload.id]
+        }
+      }, errorSpec(action))
+    }
+  }
+}
+
+const Options = types.Options.extend({
   Resource: types.ResourceType,
-  idField: Tc.String,
   methods: Tc.maybe(Tc.list(Tc.String)),
 }, 'Options')
 
 const Reducer = Tc.Function
 
 module.exports = Tc.func(
-  Options, Reducer,'createReducer'
-).of(createActionTypes)
-
-module.exports = createReducer
-
-function createActionTypes (options) {
+  Options, Reducer, 'createReducer'
+).of(createReducer)
 
 
 function createReducers (options) {
   options = util.setDefaults(Options, options, {
-    methods: constants.METHODS,
-    sections: constants.SECTIONS,
     idField: constants.DEFAULT_ID_FIELD,
+    idType: types.Id
   })
 
   const actionTypes = createActionTypes(options)
-  const payloads = createActionPayloadType(options)
+  const payloadTypes = createPayloadTypes(options)
+
+  const Resource = options.Resource
+  const idField = options.idField
+  const Id = options.idType
+
+  const service = toCamelCase(Resource.meta.name)
+  const Data = Resource.meta.type
+
+  const StartPayload = sectionUnion(service, payloadTypes, 'start')
+  const SuccessPayload = sectionUnion(service, payloadTypes, 'success')
+  const ErrorPayload = sectionUnion(service, payloadTypes, 'error')
 
   const Records = Tc.dict(Id, Data)
   const Pending = Tc.dict(Cid, StartPayload)
@@ -47,133 +186,38 @@ function createReducers (options) {
     error: Error,
   })
 
-  const specCreators = {
-    [actionTypes.findStart]: function (action) {
-      return startSpec(action)
-    },
-    [actionTypes.findSuccess]: function (action) {
-      return Object.assign({
-        records: action.payload.body.reduce(function (sofar, entity) {
-          sofar[entity[key]] = { $set: entity }
-          return sofar
-        }, {})
-      }, successSpec(action))
-    },
-    [actionTypes.findError]: function (action) {
-      return errorSpec(action)
-    },
-    [actionTypes.getStart]: function (action) {
-      return startSpec(action)
-    },
-    [actionTypes.getSuccess]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.id]: { $set: action.payload.body }
-        }
-      }, successSpec(action))
-    },
-    [actionTypes.getError]: function (action) {
-      return errorSpec(action)
-    },
-    [actionTypes.createStart]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.cid]: { $set: action.payload.data }
-        }
-      }, startSpec(action))
-    },
-    [actionTypes.createSuccess]: function (action) {
-      return Object.assign({
-        records: {
-          $remove: [action.payload.cid],
-          [action.payload.body[key]]: { $set: action.payload.body }
-        }
-      }, successSpec(action))
-    },
-    [actionTypes.createError]: function (action) {
-      return Object.assign({
-        records: {
-          $remove: [action.payload.cid]
-        }
-      }, errorSpec(action))
-    },
-    [actionTypes.updateStart]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.id]: { $set: action.payload.data }
-        }
-      }, startSpec(action))
-    },
-    [actionTypes.updateSuccess]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.id]: { $set: action.payload.body }
-        }
-      }, successSpec(action))
-    },
-    [actionTypes.updateError]: function (action) {
-      return Object.assign({
-        // TODO reset to before update
-        records: {
-          $remove: [action.payload.id]
-        }
-      }, errorSpec(action))
-    },
-    [actionTypes.patchStart]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.id]: { $merge: action.payload.data }
-        }
-      }, startSpec(action))
-    },
-    [actionTypes.patchSuccess]: function (action) {
-      return Object.assign({
-        records: {
-          [action.payload.id]: { $merge: action.payload.body }
-        }
-      }, successSpec(action))
-    },
-    [actionTypes.patchError]: function (action) {
-      return Object.assign({
-        // TODO reset to before patch
-        records: {
-          $remove: [action.payload.id]
-        }
-      }, errorSpec(action))
-    },
-    [actionTypes.removeStart]: function (action) {
-      return Object.assign({
-        records: {
-          $remove: [action.payload.id]
-        }
-      }, startSpec(action))
-    },
-    [actionTypes.removeSuccess]: function (action) {
-      return Object.assign({
-        records: {
-          $remove: [action.payload.id]
-        }
-      }, successSpec(action))
-    },
-    [actionTypes.removeError]: function (action) {
-      return Object.assign({
-        // TODO reset to before remove
-        records: {
-          $remove: [action.payload.id]
-        }
-      }, errorSpec(action))
-    }
-  }
+  const reducers = mapValues(actionTypes, function (sections, method) {
+    mapValues(sections, function (actionType, section) {
+      const specCreator = specCreators[method][section]
+      const actionId = 
 
-  const actionHandlers = mapValues(specCreators, (specCreator) => {
-    return function (state, action) {
-      return State.update(state, specCreator(action))
-    }
+      return Tc.func([State, actionType], State)
+        .of(createReducer(State, actionId, specCreator))
+    })
   })
 
-  return handleActions(actionHandlers, {
+  const reducer = flatten(values(mapValues(reducer, values))))
+
+  return defaultReducer(reducer, {
     records: {}
   })
+}
+
+function defaultReducer (reducer, defaultState) {
+  return function (state, action) {
+    if (isUndefined(state)) {
+      state = defaultState
+    }
+    return reducer(state, action)
+  }
+}
+
+function createReducer (State, actionId, specCreator) {
+  return function (state, action) {
+    return (action.type === actionId) ?
+      State.update(state, specCreator(action))
+      : state
+  }
 }
 
 function startSpec (action) {
@@ -194,4 +238,11 @@ function errorSpec (action) {
     pending: { $remove: [action.payload.cid] },
     error: { $set: { [action.payload.cid]: action } }
   }
+}
+
+function sectionUnion (service, payloadTypes, section) {
+  const sectionTypes = values(mapValues(payloadTypes, section))
+  const sectionTypeName = toCapitalCase(service, section, 'payload')
+
+  return Tc.union(sectionTypes, sectionTypeName)
 }
