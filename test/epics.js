c08735b8926d65f01f7a045fa4c7f0a9c89ebdec
Piet Geursen <pietgeursen@gmail.com>
2017-04-18 17:00:53 +1200

    Adds some tests for epics

--- test/epics.js
+++ test/epics.js
@@ -9,8 +9,76 @@ test('FEATHERS_ACTION is handled by epic and emits something', function(t) {
   const action$ = Rx.Observable.of(action)
 
   epics(action$)  
-    .subscribe(() => {
-      t.pass() 
+    .subscribe((action) => {
+      t.ok(action) 
+      t.end()
+    })
+})
+
+test('create is handled by epic and calls create on the client', function(t) {
+  const { action, epics } = cats.actions.create({name: 'fluffy'})
+  const action$ = Rx.Observable.of(action)
+
+  const client = {
+    create: () => {
+      t.pass()
+      t.end()
+    } 
+  }
+
+  epics(action$, {}, client)  
+    .subscribe((action) => {
+    })
+})
+
+test('create is handled by epic and emits request start action', function(t) {
+  const { action, epics } = cats.actions.create({name: 'fluffy'})
+  const action$ = Rx.Observable.of(action)
+
+  const client = {
+    create: () => {
+    } 
+  }
+
+  epics(action$, {}, client)  
+    .filter({type} => type === 'FEATHERS_REQUEST_START' )
+    .subscribe((action) => {
+      t.ok(action)
+      t.end()
+    })
+})
+
+test('create is handled by epic and emits set action', function(t) {
+  const { action, epics } = cats.actions.create({name: 'fluffy'})
+  const action$ = Rx.Observable.of(action)
+
+  const client = {
+    create: () => {
+    } 
+  }
+
+  epics(action$, {}, client)  
+    .filter({type} => type === 'FEATHERS_CATS_SET' )
+    .subscribe((action) => {
+      t.ok(action)
+      t.end()
+    })
+})
+
+test('create is handled by epic and emits set action', function(t) {
+  const { action, epics } = cats.actions.create({name: 'fluffy'})
+  const action$ = Rx.Observable.of(action)
+
+  const client = {
+    create: (cat) => {
+      return Promise.resolve(cat)
+    } 
+  }
+  //TODO: collect up the actions?
+  epics(action$, {}, client)
+    .filter({type} => type === 'FEATHERS_CATS_SET' )
+    .subscribe((action) => {
+      t.ok(action)
       t.end()
     })
 })
