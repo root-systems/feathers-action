00212d845fa27621e9e1be67cab553aa15b174ff
Piet Geursen <pietgeursen@gmail.com>
2017-04-27 14:28:36 +1200

    adding better integrations tests

--- test/app.js
+++ test/app.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var feathers = require('feathers')
+var { keys, any, pipe } = require('ramda')
 var memory = require('feathers-memory')
 var {createStore, applyMiddleware} = require('redux')
 var { createEpicMiddleware, combineEpics } = require('redux-observable')
@@ -27,7 +28,11 @@ test('create, update, patch and remove update the store', function(t) {
 
   const store = createStore(reducer, applyMiddleware(epicMiddleware))
 
-  
+  const createCid = Cid()
+  const updateCid = Cid()
+  const patchCid = Cid()
+  const removeCid = Cid()
+
   Store$(store) 
     .filter((store) => store.cats.cats[0])
     .take(1)
@@ -57,8 +62,45 @@ test('create, update, patch and remove update the store', function(t) {
       t.end()
     })
 
-  store.dispatch(cats.actions.create(Cid(), {name: 'fluffy'}))
+  store.dispatch(cats.actions.create(createCid, {name: 'fluffy'}))
 
+})
+
+test('create optimistically updates store and then sets the id when request succeeds', function(t) {
+
+  const app = createApp(serviceNames)
+  const {cats, dogs} = createModule(serviceNames)
+  const catActions = cats.actions
+
+  const updaters = reduxFp.combine({cats: cats.updater, dogs: dogs.updater}) 
+  const epics = combineEpics(cats.epic, dogs.epic)
+
+  const reducer = (state, action) => updaters(action)(state)
+
+  const epicMiddleware = createEpicMiddleware(epics, {dependencies: {feathers: app}})
+
+  const store = createStore(reducer, applyMiddleware(epicMiddleware))
+
+  const createCid = Cid()
+  const keysHaveCidKey = pipe(keys, any(key => key === createCid))
+
+  Store$(store) 
+    .filter((store) => store.cats.cats[createCid])
+    .take(1)
+    .subscribe(({cats}) => {
+      t.equal(cats.cats[createCid].name, 'fluffy', 'Cat is created optimistically') 
+    })
+
+  Store$(store) 
+    .filter((store) => store.cats.cats[0])
+    .take(1)
+    .subscribe(({cats}) => {
+      t.equal(cats.cats[0].name, 'fluffy', 'Cat is set at the expected id') 
+      t.false(keysHaveCidKey(cats.cats), 'Temporary cid key is deleted')
+      t.end()
+    })
+
+  store.dispatch(cats.actions.create(createCid, {name: 'fluffy'}))
 })
 
 function Store$ (store) {
